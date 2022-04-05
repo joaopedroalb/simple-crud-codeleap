a04c042ae22eafffdecec86074019c117f37e875
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-04-05 12:39:45 -0300

    feat: add logic to navigate to home in login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next'
+import Login from './login'
 
 const Home: NextPage = () => {
+
+  if(false)
+    return <Login/>
+
   return (
     <div>
       Hello World
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,26 @@
-import {} from 'react'
-import {Container,LoginBox,BtnContainer} from '../styles/Login'
-import {Title,Label,Input, Button} from '../styles/defaultComponents'
+import {} from "react";
+import { Container, LoginBox, BtnContainer } from "../styles/Login";
+import { Title, Label, Input, Button } from "../styles/defaultComponents";
+import { useRouter } from "next/router";
 
 export default function Login() {
-  return (
+
+    const router = useRouter();
+
+    const handleClick = () =>{
+        router.push('/')
+    }
+    
+    return (
     <Container>
         <LoginBox>
-            <Title>Welcome to CodeLeap network!</Title>
-            <Label>Please enter your username</Label>
-            <Input placeholder='John doe'/>
-            <BtnContainer>
-                <Button>ENTER</Button>
-            </BtnContainer>
+        <Title>Welcome to CodeLeap network!</Title>
+        <Label>Please enter your username</Label>
+        <Input placeholder="John doe" />
+        <BtnContainer>
+            <Button onClick={handleClick}>ENTER</Button>
+        </BtnContainer>
         </LoginBox>
     </Container>
-  )
+    );
 }
